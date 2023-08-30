ac0c06389cb69756c4e3af2d57ddb6a9ea4dee10
Shivangi Dhakad <85348968+shivangidhakad15@users.noreply.github.com>
2023-08-30 16:23:37 +0530

    Time: 5 ms (55.88%), Space: 42 MB (85.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,26 +4,44 @@ class Solution {
         if (s.length() != t.length()){
             return false;
         }
+        int count[] = new int[26];
 
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-        for (int i = 0; i < t.length(); i++){
-            char ch = t.charAt(i);
-            if (map.get(ch) != null){
-                if (map.get(ch) == 1){
-                    map.remove(ch);
-                }
-                else{
-                    map.put(ch, map.get(ch) - 1);
-                }
-            }
-            else{
+        for(int i = 0; i < s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+        } 
+        for(int i = 0; i < t.length(); i++){
+            count[t.charAt(i) - 'a']--;
+        } 
+        for(int i = 0; i < count.length; i++){
+            if(count[i] != 0){
                 return false;
             }
         }
-        return map.isEmpty();
+        return true;
+
+
+
+
+
+        // HashMap<Character, Integer> map = new HashMap<>();
+        // for (int i = 0; i < s.length(); i++){
+        //     char ch = s.charAt(i);
+        //     map.put(ch, map.getOrDefault(ch, 0) + 1);
+        // }
+        // for (int i = 0; i < t.length(); i++){
+        //     char ch = t.charAt(i);
+        //     if (map.get(ch) != null){
+        //         if (map.get(ch) == 1){
+        //             map.remove(ch);
+        //         }
+        //         else{
+        //             map.put(ch, map.get(ch) - 1);
+        //         }
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        // }
+        // return map.isEmpty();
     }
 }
