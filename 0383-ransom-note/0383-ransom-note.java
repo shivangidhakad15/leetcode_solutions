4f56e2898d1c1decad13ddfbc216d7ef583f898e
Shivangi Dhakad <85348968+shivangidhakad15@users.noreply.github.com>
2023-10-08 17:52:19 +0530

    Time: 2 ms (92.68%), Space: 43 MB (99.07%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,21 +1,12 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> dictionary = new HashMap<>();
-        for (int i = 0; i < magazine.length(); i++) {
-            char c = magazine.charAt(i);
-            if (!dictionary.containsKey(c)) {
-                dictionary.put(c, 1);
-            } else {
-                dictionary.put(c, dictionary.get(c) + 1);
-            }
-        }
-        for (int i = 0; i < ransomNote.length(); i++) {
-            char c = ransomNote.charAt(i);
-            if (dictionary.containsKey(c) && dictionary.get(c) > 0) {
-                dictionary.put(c, dictionary.get(c) - 1);
-            } else {
-                return false;
-            }
+        int[] count = new int[26];
+        for(char ch : magazine.toCharArray())
+            count[ch-'a']++;
+        for(char ch : ransomNote.toCharArray()){
+            count[ch-'a']--;
+            if(count[ch-'a'] <0)
+            return false;
         }
         return true;
     }
