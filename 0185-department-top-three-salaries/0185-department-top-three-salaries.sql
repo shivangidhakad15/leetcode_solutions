dcbe2004dec86a2c058f9c176ac4528cbd7815ba
Shivangi Dhakad <85348968+shivangidhakad15@users.noreply.github.com>
2023-10-18 23:25:52 +0530

    Time: 2263 ms (38.75%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
 
-SELECT d.name as department, e1.name as employee, e1.salary as salary FROM Employee as e1 INNER JOIN Department as d ON e1.departmentId = d.id WHERE 3 > (SELECT COUNT(distinct e2.salary) FROM Employee as e2 WHERE e2.salary > e1.salary AND e1.departmentId = e2.departmentId);
+SELECT d.name AS department, e1.name AS employee, e1.salary AS salary FROM Employee AS e1 INNER JOIN Department AS d ON e1.departmentId = d.id WHERE 3 > (SELECT COUNT(DISTINCT e2.salary) FROM Employee AS e2 WHERE e2.salary > e1.salary AND e1.departmentId = e2.departmentId);
