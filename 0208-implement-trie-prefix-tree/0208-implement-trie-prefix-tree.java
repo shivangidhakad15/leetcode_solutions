5bed0a98cbf4db3c3f321370c0c9cdfb255ed1ff
Shivangi Dhakad <85348968+shivangidhakad15@users.noreply.github.com>
2024-02-08 20:19:54 +0530

    Time: 35 ms (76.13%), Space: 55.1 MB (67.75%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,21 +1,57 @@
-class Trie {
-    Map<String, Boolean> map;
-    public Trie() {
-        map = new HashMap<>();
-    }
-    public void insert(String word) {
-        map.put(word, true);
-    }
-    public boolean search(String word) {
-        return map.getOrDefault(word, false);
-    }
-    public boolean startsWith(String prefix) {
-        for (String word : map.keySet()) {
-            if (word.startsWith(prefix)) {
-                return true;
+class Trie { 
+    static class TreeNode {
+        TreeNode[] children = new TreeNode[26];
+        boolean theEnd;
+
+        public TreeNode(){
+            this.theEnd = false;
+            for (int i = 0; i < children.length; i++) {
+                children[i] = null;
             }
         }
-        return false;
+    }
+    TreeNode root;
+    public Trie() {
+        this.root = new TreeNode();
+    }
+    public void insert(String word) {
+        int n = word.length();
+        int index;
+        TreeNode temp = root;
+        for (int i = 0; i < n; i++) {
+            index = word.charAt(i) - 'a';
+            if(temp.children[index] == null){
+                temp.children[index] = new TreeNode();
+            }
+            temp = temp.children[index];
+        }
+        temp.theEnd = true;
+    }
+    public boolean search(String word) {
+        int n = word.length();
+        int index;
+        TreeNode temp = root;
+        for (int i = 0; i < n; i++) {
+            index = word.charAt(i) - 'a';
+            if(temp.children[index] == null){
+                return false;
+            }
+            temp = temp.children[index];
+        }
+        return temp.theEnd;    
+    }
+    public boolean startsWith(String prefix) {
+        int n = prefix.length();
+        int index;
+        TreeNode temp = root;
+        for (int i = 0; i < n; i++) {
+            index = prefix.charAt(i) - 'a';
+            if(temp.children[index] == null){
+                return false;
+            }
+            temp = temp.children[index];
+        }
+        return true;
     }
 }
 
