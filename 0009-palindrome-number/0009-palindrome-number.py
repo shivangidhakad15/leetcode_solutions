495704199adbe037a062d7dce4d0bc06ce639d9c
Shivangi Dhakad <85348968+shivangidhakad15@users.noreply.github.com>
2024-06-07 02:05:14 +0530

    Time: 51 ms (68.02%), Space: 16.6 MB (56.53%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,7 +1,22 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        s = str(x)
-        reverse = s[::-1]  
-        if reverse == s:
+        # s = str(x)
+        # reverse = s[::-1]  
+        # if reverse == s:
+        #     return True
+        # return False
+
+        if x < 0:
+            return False
+        if x == 0:  # special case
             return True
-        return False
+        if x % 10 == 0: # x mod 10 which is getting the last digit and the last digit is 0, then we also know that it's not a palindrome (ex: 30 is not a palindrome)
+            return False
+
+        originalX = x
+        numReversed = 0
+        while x > 0:
+            lastDigit = x % 10
+            numReversed = numReversed * 10 + lastDigit
+            x = x // 10
+        return numReversed == originalX
