14656bfa1176928c87d2e345e6f8e97a2a410cec
Shivangi Dhakad <85348968+shivangidhakad15@users.noreply.github.com>
2024-07-17 19:49:31 +0530

    Time: 30 ms (99.64%), Space: 17 MB (28.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        return Counter(s) == Counter(t)
+        # if len(s) != len(t):
+        #     return False
 
-        s = sorted(s)
-        t = sorted(t)
-        if s == t:
-            return True
-        else:
-            return False 
+        # s = sorted(s)
+        # t = sorted(t)
+        # if s == t:
+        #     return True
+        # else:
+        #     return False 
         
 
         
